01b2c4d998159cd02e2f642dd387c17088a1eb86
chandra7499 <chandranomula259@gmail.com>
2026-02-13 19:43:33 +0530

    minor update

--- src/lib/algolia_client.js
+++ src/lib/algolia_client.js
@@ -1,9 +1,10 @@
 export const runtime = "nodejs";
-import algoliasearch from "algoliasearch/lite";
- 
+
+import algoliasearch from "algoliasearch";
+
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY, // 🔒 search-only key
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY
 );
 
 export default searchClient;
